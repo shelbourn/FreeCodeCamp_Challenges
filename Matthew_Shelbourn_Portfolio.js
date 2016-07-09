cdd6273be999ee3f87af6e138e200a5ab2ef2fb4
shelbourn <mattshelbourn@gmail.com>
2016-07-09 12:37:57 -0700

    Noting changes made to portfolio files.

--- Matthew_Shelbourn_Portfolio.js
+++ Matthew_Shelbourn_Portfolio.js
@@ -4,3 +4,10 @@ $(".nav a").on("click", function(){
   $(".nav").find(".active").removeClass("active");
   $(this).parent().addClass("active");
 });
+
+//Collapsible Navbar
+
+$('.collapse').collapse()
+
+$('.navbar-collapse ul li a').click(function() {
+  $(".navbar-collapse").collapse('hide');});
