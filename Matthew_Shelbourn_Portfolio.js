b30770532225ca96b88c52ec3e1b0df8bbdecce5
shelbourn <mattshelbourn@gmail.com>
2016-07-09 23:55:10 -0700

    Updated all files with last couple hours of progress. Finally got the collapsible navbar to work.

--- Matthew_Shelbourn_Portfolio.js
+++ Matthew_Shelbourn_Portfolio.js
@@ -1,13 +1,15 @@
 // Changes "active" class while navigating
 
-$(".nav a").on("click", function(){
-  $(".nav").find(".active").removeClass("active");
-  $(this).parent().addClass("active");
+$(".nav a").on("click", function() {
+	$(".nav").find(".active").removeClass("active");
+	$(this).parent().addClass("active");
 });
 
 //Collapsible Navbar
 
+/*
 $('.collapse').collapse()
 
 $('.navbar-collapse ul li a').click(function() {
   $(".navbar-collapse").collapse('hide');});
+	*/
