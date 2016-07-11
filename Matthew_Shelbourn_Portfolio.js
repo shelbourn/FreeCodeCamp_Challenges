7d02708252f870a9d050e882b353ca8faca51bf2
shelbourn <mattshelbourn@gmail.com>
2016-07-10 22:31:31 -0700

    Updating FCC portfolio page HTML, JS, and CSS files. This project is completed for now.

--- Matthew_Shelbourn_Portfolio.js
+++ Matthew_Shelbourn_Portfolio.js
@@ -13,3 +13,24 @@ $('.collapse').collapse()
 $('.navbar-collapse ul li a').click(function() {
   $(".navbar-collapse").collapse('hide');});
 	*/
+
+//Animate on Hover
+$(document).ready(function(){
+    $('#logo').each(function() {
+        animationHover(this, 'bounce');
+    });
+});
+
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);        
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);         
+        });
+}
