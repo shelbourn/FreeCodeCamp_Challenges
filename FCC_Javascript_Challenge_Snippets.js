34a92c82050590234f81e8e9df0e8190a3cdac95
shelbourn <mattshelbourn@gmail.com>
2016-07-20 15:15:57 -0700

    Updating JS notes file.

--- FCC_Javascript_Challenge_Snippets.js
+++ FCC_Javascript_Challenge_Snippets.js
@@ -745,5 +745,43 @@ function chainToSwitch(val) {
 // Change this value to test
 chainToSwitch(7);
 
+/* From Challenge: "Returning Boolean Values from Functions"
+
+You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+
+A common anti-pattern is to use an if/else statement to do a comparison and then return true/false:
+
+function isEqual(a,b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+Since === returns true or false, we can return the result of the comparison:
+
+function isEqual(a,b) {
+  return a === b;
+}
+Instructions
+Fix the function isLess to remove the if/else statements. */
+
+// Before
+
+
+function isLess(a, b) {
+  // Fix this code
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Change these values to test
+isLess(10, 15);
+
+//After
+
 
 
