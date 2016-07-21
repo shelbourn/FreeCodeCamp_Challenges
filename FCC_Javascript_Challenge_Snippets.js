2ce47e4eccdf703a1f08ac82f1ca717d07d997c6
shelbourn <mattshelbourn@gmail.com>
2016-07-21 16:51:55 -0700

    Updating JS notes file.

--- FCC_Javascript_Challenge_Snippets.js
+++ FCC_Javascript_Challenge_Snippets.js
@@ -1125,3 +1125,57 @@ function phoneticLookup(val) {
 
 // Change this value to test
 phoneticLookup("charlie");
+
+/* From Challenge: "Testing Objects for Properties"
+
+Sometimes it is useful to check if the property of a given object exists or not. We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not.
+
+Example
+
+var myObj = {
+  top: "hat",
+  bottom: "pants"
+};
+myObj.hasOwnProperty("top");    // true
+myObj.hasOwnProperty("middle"); // false
+Instructions
+Modify the function checkObj to test myObj for checkProp. If the property is found, return that property's value. If not, return "Not Found". */
+
+// Before
+
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+  
+  return "Change Me!";
+}
+
+// Test your code by modifying these values
+checkObj("gift");
+
+// After
+
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+  if (myObj.hasOwnProperty(checkProp) === true)
+  return myObj[checkProp];
+  else
+    return "Not Found";
+  
+}
+
+// Test your code by modifying these values
+checkObj("gift");
