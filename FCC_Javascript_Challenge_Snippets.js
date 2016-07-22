1a2003aee79b37a1b734d8da52f132ae21a0ed4c
shelbourn <mattshelbourn@gmail.com>
2016-07-21 17:20:57 -0700

    Updating JS notes file.

--- FCC_Javascript_Challenge_Snippets.js
+++ FCC_Javascript_Challenge_Snippets.js
@@ -1179,3 +1179,91 @@ function checkObj(checkProp) {
 
 // Test your code by modifying these values
 checkObj("gift");
+
+/* From Challenge: "Manipulating Complex Objects"
+
+Sometimes you may want to store data in a flexible Data Structure. A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
+
+Here's an example of a complex data structure:
+
+var ourMusic = [
+  {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [ 
+      "CD", 
+      "Cassette", 
+      "LP" ],
+    "gold": true
+  }
+];
+This is an array which contains one object inside. The object has various pieces of metadata about an album. It also has a nested "formats" array. If you want to add more album records, you can do this by adding records to the top level array.
+
+Objects hold data in a property, which has a key-value format. In the example above, "artist": "Daft Punk" is a property that has a key of "artist" and a value of "Daft Punk".
+
+JavaScript Object Notation or JSON is a related data interchange format used to store data.
+
+{
+  "artist": "Daft Punk",
+  "title": "Homework",
+  "release_year": 1997,
+  "formats": [ 
+    "CD",
+    "Cassette",
+    "LP"
+  ],
+  "gold": true
+}
+Note
+You will need to place a comma in between every object in the array, unless it is the last object in the array.
+
+Instructions
+Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings. */
+
+// JSON Site: http://www.json.org/
+
+// Before
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  }
+  // Add record here
+];
+
+// After
+
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  
+  {
+    "artist": "OAR",
+    "title": "Dareh Meyod",
+    "release_year": 2004,
+    "formats": [
+      "CD",
+      "LP",
+      "Digital Download"
+    ]
+  }
+];
+
+
